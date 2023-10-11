2668ba734d110836420c929b424115363c3dcc95
Shawn Werber <shawnthewerber@gmail.com>
2023-10-11 16:44:52 -0600

    Remove file, start express add
    Remove unneeded files as part of the transition to express endpoints

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,102 +7,144 @@ import bookmark from '../../assets/bookmark.png'
 
 const Home = () => {
   const { currentUser, equipment, rentItem, saveItem, savedForLater, activeRentals, searchText, loggedIn, loginModalSwitch, setAuthModalType } = useRent();
-  const remainingEquipment = equipment.filter((item) => !item.isRented);
+  const remainingEquipment =
+    equipment?.filter((item) => !item.isRented) || null;
   const [productModal, setProductModal] = useState(false)
   const [currentItem, setCurrentItem] = useState({})
 
   return (
     <>
       <div className='hero-wrapper'>
-        <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcapitalremanexchange.com%2Fwp-content%2Fuploads%2F2017%2F02%2FRental-3-1024x683.jpg&f=1&nofb=1&ipt=a60a93ed07dc5d0bf0e4c8148ef2910ef810e87ee01557b9f0fc20362cc426e1&ipo=images" alt="Equipment Hero Image" />
+        <img
+          src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcapitalremanexchange.com%2Fwp-content%2Fuploads%2F2017%2F02%2FRental-3-1024x683.jpg&f=1&nofb=1&ipt=a60a93ed07dc5d0bf0e4c8148ef2910ef810e87ee01557b9f0fc20362cc426e1&ipo=images'
+          alt='Equipment Hero Image'
+        />
       </div>
       <h2>Available Equipment</h2>
       <div className='equipment-gallery'>
-        {equipment.length && remainingEquipment.map((item) => {
-          let bookmarkClass = '';
-          if(currentUser !== null && savedForLater.filter((savedItem) => savedItem.userId === currentUser.id && savedItem.equipmentId === item.id).length) {
-            bookmarkClass = 'bookmark'
-          } else bookmarkClass = 'bookmark hidden'
-          if(item.name.toLowerCase().includes(searchText)) {
-            return <div className='equipment-gallery-item' key={item.id}>
-              <img className={bookmarkClass} src={bookmark} alt="bookmark" />
-              <h3>{item.name}</h3>
-              <img src={item.image} alt={item.name} onClick={() => {
-                setProductModal(true)
-                setCurrentItem(item)
-              }}/>
-              <button onClick={() => {
-                setProductModal(true)
-                setCurrentItem(item)
-              }}>More Info</button>
-            </div>
-          }
-        })}
+        {equipment?.length &&
+          remainingEquipment.map((item) => {
+            let bookmarkClass = '';
+            if (
+              currentUser !== null &&
+              savedForLater.filter(
+                (savedItem) =>
+                  savedItem.userId === currentUser.id &&
+                  savedItem.equipmentId === item.id
+              ).length
+            ) {
+              bookmarkClass = 'bookmark';
+            } else bookmarkClass = 'bookmark hidden';
+            if (item.name.toLowerCase().includes(searchText)) {
+              return (
+                <div className='equipment-gallery-item' key={item.id}>
+                  <img
+                    className={bookmarkClass}
+                    src={bookmark}
+                    alt='bookmark'
+                  />
+                  <h3>{item.name}</h3>
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    onClick={() => {
+                      setProductModal(true);
+                      setCurrentItem(item);
+                    }}
+                  />
+                  <button
+                    onClick={() => {
+                      setProductModal(true);
+                      setCurrentItem(item);
+                    }}
+                  >
+                    More Info
+                  </button>
+                </div>
+              );
+            }
+          })}
       </div>
-      {productModal && 
-        <div className="product-modal">
+      {productModal && (
+        <div className='product-modal'>
           <img src={x} onClick={() => setProductModal(false)}></img>
           <h3>{currentItem.name}</h3>
-          <img className='product-image' src={currentItem.image} alt={currentItem.name} />
+          <img
+            className='product-image'
+            src={currentItem.image}
+            alt={currentItem.name}
+          />
           <p>{currentItem.description}</p>
-          <div className="item-btn-wrapper">
-            <button onClick={async () => {
-              if(loggedIn) {
-                setProductModal(false)
-                const currentRentals = activeRentals.length;
-                console.log('before', currentRentals);
-                await rentItem(currentItem)
-                console.log('after', activeRentals.length);
-                if(currentRentals < activeRentals.length) {
-                  toast.success('Equipment Rented', {
-                    position: "top-center",
-                    autoClose: 2000,
-                    hideProgressBar: true,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "dark",
-                  })
-                } else {
-                  toast.error('Error Submitting Rental', {
-                    position: "top-center",
-                    autoClose: 2000,
-                    hideProgressBar: true,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "dark",
-                  })
-                }
-              } else {
-                setAuthModalType('signin')
-                setProductModal(false)
-                loginModalSwitch()
-              }
-            }}>Rent</button>
-            
-            {currentUser !== null && savedForLater.filter((item) => {
-                if(item.userId === currentUser.id && 
-                item.equipmentId === currentItem.id){ 
-                  return item 
-                }
-              }).length ? 
-                <h3 className='saved-notice'>Item Already Saved</h3>
-              : <button onClick={() => {
-                if(loggedIn) {
-                    saveItem(currentItem)
+          <div className='item-btn-wrapper'>
+            <button
+              onClick={async () => {
+                if (loggedIn) {
+                  setProductModal(false);
+                  const currentRentals = activeRentals.length;
+                  console.log('before', currentRentals);
+                  await rentItem(currentItem);
+                  console.log('after', activeRentals.length);
+                  if (currentRentals < activeRentals.length) {
+                    toast.success('Equipment Rented', {
+                      position: 'top-center',
+                      autoClose: 2000,
+                      hideProgressBar: true,
+                      closeOnClick: true,
+                      pauseOnHover: true,
+                      draggable: true,
+                      progress: undefined,
+                      theme: 'dark',
+                    });
                   } else {
-                    setAuthModalType('signin')
-                    setProductModal(false)
-                    loginModalSwitch()
+                    toast.error('Error Submitting Rental', {
+                      position: 'top-center',
+                      autoClose: 2000,
+                      hideProgressBar: true,
+                      closeOnClick: true,
+                      pauseOnHover: true,
+                      draggable: true,
+                      progress: undefined,
+                      theme: 'dark',
+                    });
                   }
-              }}>Save for Later</button>
-            }
+                } else {
+                  setAuthModalType('signin');
+                  setProductModal(false);
+                  loginModalSwitch();
+                }
+              }}
+            >
+              Rent
+            </button>
+
+            {currentUser !== null &&
+            savedForLater.filter((item) => {
+              if (
+                item.userId === currentUser.id &&
+                item.equipmentId === currentItem.id
+              ) {
+                return item;
+              }
+            }).length ? (
+              <h3 className='saved-notice'>Item Already Saved</h3>
+            ) : (
+              <button
+                onClick={() => {
+                  if (loggedIn) {
+                    saveItem(currentItem);
+                  } else {
+                    setAuthModalType('signin');
+                    setProductModal(false);
+                    loginModalSwitch();
+                  }
+                }}
+              >
+                Save for Later
+              </button>
+            )}
           </div>
         </div>
-      }
+      )}
     </>
   );
 }
--- src/providers/rent_provider.jsx
+++ src/providers/rent_provider.jsx
@@ -1,9 +1,15 @@
-import React, { useState, useEffect, createContext, useContext } from 'react';
+import React, {
+  useState,
+  useEffect,
+  createContext,
+  useContext,
+  useRef,
+} from 'react';
 
 const RentContext = createContext({});
 
 // eslint-disable-next-line react/prop-types
-export const RentProvider = ({children}) => {
+export const RentProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState({});
   const [equipment, setEquipment] = useState([]);
   const [savedForLater, setSavedForLater] = useState([]);
@@ -14,197 +20,208 @@ export const RentProvider = ({children}) => {
   const [authModalType, setAuthModalType] = useState('login');
   const [searchText, setSearchText] = useState('');
 
+  const renderChecker = useRef(false);
+
   const fetchDb = async (url) => {
     try {
-      const response = await fetch(url)
-      const jsonResponse = await response.json()
+      const response = await fetch(url);
+      const jsonResponse = await response.json();
       return jsonResponse;
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchDb('http://localhost:3000/equipment').then(result => {
-      setEquipment(result);
-    });
-    fetchDb('http://localhost:3000/savedForLater').then(result => {
-      setSavedForLater(result);
-    });
-    fetchDb('http://localhost:3000/activeRentals').then(result => {
-      setActiveRentals(result);
-    });
+    if (renderChecker.current) {
+      fetchDb(`http://localhost:3000/equipment`).then((result) => {
+        setEquipment(result);
+      });
+      fetchDb(`http://localhost:3000/saved:${currentUser}`).then((result) => {
+        setSavedForLater(result);
+      });
+      fetchDb(`http://localhost:3000/rentals/:${currentUser}`).then(
+        (result) => {
+          setActiveRentals(result);
+        }
+      );
 
-    let localUser = localStorage.getItem('user');
-    if(localUser !== null && localUser !== 'undefined') {
-      setCurrentUser(JSON.parse(localUser))
-      setLoggedIn(true)
+      let localUser = localStorage.getItem('user');
+      if (localUser !== null && localUser !== 'undefined') {
+        setCurrentUser(JSON.parse(localUser));
+        setLoggedIn(true);
+      }
+
+      renderChecker.current = true;
     }
   }, []);
 
   const rentItem = async (item) => {
-    if(loggedIn) {
+    if (loggedIn) {
       await addActiveRental(item, currentUser.id);
     } else setLoginModal(true);
-  }
+  };
 
-  const addActiveRental = async (item, idOfUser) => {
+  const addActiveRental = async (item, userEmail) => {
     const newestAddition = {
-      userId: idOfUser,
+      userId: userEmail,
       equipmentId: item.id,
-    }
+    };
     try {
-      await fetch(`http://localhost:3000/activeRentals`, { 
-        method: 'POST', 
+      await fetch(`http://localhost:3000/rentals`, {
+        method: 'POST',
         body: JSON.stringify(newestAddition),
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
         },
-      })
+      });
       await fetch(`http://localhost:3000/equipment/${item.id}`, {
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-        method: "PATCH",
+        method: 'PATCH',
         body: JSON.stringify({
           isRented: true,
         }),
-      })
-      await fetchDb('http://localhost:3000/activeRentals').then(result => {
+      });
+      await fetchDb('http://localhost:3000/rentals').then((result) => {
         setActiveRentals(result);
-      })
-      await fetchDb('http://localhost:3000/equipment').then(result => {
+      });
+      await fetchDb('http://localhost:3000/equipment').then((result) => {
         setEquipment(result);
-      })
+      });
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   const saveItem = (item) => {
-    if(loggedIn) {
+    if (loggedIn) {
       addSavedForLater(item, currentUser.id);
     } else setLoginModal(true);
-  }
+  };
 
   const addSavedForLater = async (item, idOfUser) => {
     const newestAddition = {
       userId: idOfUser,
       equipmentId: item.id,
-    }
+    };
     try {
-      await fetch(`http://localhost:3000/savedForLater`, { 
-        method: 'POST', 
+      await fetch(`http://localhost:3000/saved/:${currentUser}`, {
+        method: 'POST',
         body: JSON.stringify(newestAddition),
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
         },
-      })
-      await fetchDb('http://localhost:3000/savedForLater').then(result => {
+      });
+      await fetchDb(`http://localhost:3000/saved/:${currentUser}`).then(
+        (result) => {
+          setSavedForLater(result);
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const changeRoute = (route) => {
+    setRoute(route);
+  };
+
+  const loginModalSwitch = () => {
+    loginModal === false ? setLoginModal(true) : setLoginModal(false);
+  };
+
+  const signOut = () => {
+    setCurrentUser({});
+    setRoute('home');
+    setLoggedIn(false);
+    localStorage.removeItem('user');
+  };
+
+  const removeRental = async (rentalId, item) => {
+    try {
+      await fetch(`http://localhost:3000/rentals/${rentalId}`, {
+        method: 'DELETE',
+      });
+      await fetch(`http://localhost:3000/equipment/${item.id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'PATCH',
+        body: JSON.stringify({
+          isRented: false,
+        }),
+      });
+      await fetchDb('http://localhost:3000/activeRentals').then((result) => {
+        setActiveRentals(result);
+      });
+      await fetchDb('http://localhost:3000/equipment').then((result) => {
+        setEquipment(result);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const removeSaved = async (savedId) => {
+    try {
+      await fetch(`http://localhost:3000/saved/${savedId}`, {
+        method: 'DELETE',
+      });
+      await fetchDb('http://localhost:3000/saved').then((result) => {
         setSavedForLater(result);
       });
     } catch (error) {
       console.error(error);
     }
-  }
-
-  const changeRoute = (route) => {
-    setRoute(route)
-  }
-
-  const loginModalSwitch = () => {
-    loginModal === false ? setLoginModal(true) : setLoginModal(false);
-  }
-
-  const signOut = () => {
-    setCurrentUser({})
-    setRoute('home')
-    setLoggedIn(false)
-    localStorage.removeItem('user')
-  }
-
-  const removeRental = async (rentalId, item) => {
-    try {
-      await fetch(`http://localhost:3000/activeRentals/${rentalId}`, { 
-          method: 'DELETE', 
-        },
-      )
-      await fetch(`http://localhost:3000/equipment/${item.id}`, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "PATCH",
-        body: JSON.stringify({
-          isRented: false,
-        }),
-      })
-      await fetchDb('http://localhost:3000/activeRentals').then(result => {
-        setActiveRentals(result);
-      })
-      await fetchDb('http://localhost:3000/equipment').then(result => {
-        setEquipment(result);
-      })
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  const removeSaved = async (savedId) => {
-    try {
-      await fetch(`http://localhost:3000/savedForLater/${savedId}`, { 
-          method: 'DELETE', 
-        },
-      )
-      await fetchDb('http://localhost:3000/savedForLater').then(result => {
-        setSavedForLater(result);
-      })
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  };
 
   const registerFetch = async (user) => {
     try {
-      await fetch(`http://localhost:3000/users`, { 
-        method: 'POST', 
+      await fetch(`http://localhost:3000/users`, {
+        method: 'POST',
         body: JSON.stringify(user),
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
         },
-      })
+      });
       let userInQuestion = [];
       await usersFetch().then((response) => {
         userInQuestion = response.filter((fetchedUser) => {
-          if(user.email === fetchedUser.email && user.password === fetchedUser.password) {
-            return user
+          if (
+            user.email === fetchedUser.email &&
+            user.password === fetchedUser.password
+          ) {
+            return user;
           }
-        })[0]
-      })
-      return userInQuestion
+        })[0];
+      });
+      return userInQuestion;
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   const addUser = async (name, phone, email, password) => {
     let newestAddition = {
-      "name": name,
-      "email": email,
-      "password": password,
-      "phone": phone,
-      "payment": {
-        "nameOnCard": "",
-        "cardNumber": "",
-        "exp": "",
-        "securityCode": ""
-      }
-    }
+      name: name,
+      email: email,
+      password: password,
+      phone: phone,
+      payment: {
+        nameOnCard: '',
+        cardNumber: '',
+        exp: '',
+        securityCode: '',
+      },
+    };
     await registerFetch(newestAddition).then((user) => {
-      localStorage.setItem('user', JSON.stringify(user))
-      setCurrentUser(user)
-      setLoggedIn(true)
-    })
-  }
+      localStorage.setItem('user', JSON.stringify(user));
+      setCurrentUser(user);
+      setLoggedIn(true);
+    });
+  };
 
   const usersFetch = async () => {
     try {
@@ -214,26 +231,30 @@ export const RentProvider = ({children}) => {
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   const checkUser = async (email, pw) => {
     const users = await usersFetch();
     const userInQuestion = users.filter((user) => {
-      if(user.email === email && user.password === pw) {
-        return user
+      if (user.email === email && user.password === pw) {
+        return user;
       }
-    })
-    if(userInQuestion.length && userInQuestion[0].password === pw && userInQuestion.length === 1) {
+    });
+    if (
+      userInQuestion.length &&
+      userInQuestion[0].password === pw &&
+      userInQuestion.length === 1
+    ) {
       setCurrentUser(userInQuestion[0]);
-      localStorage.setItem('user', JSON.stringify(userInQuestion[0]))
+      localStorage.setItem('user', JSON.stringify(userInQuestion[0]));
       setLoggedIn(true);
       return true;
     }
     return false;
-  }
+  };
 
   return (
-    <RentContext.Provider 
+    <RentContext.Provider
       value={{
         loginModal,
         setLoginModal,
@@ -255,12 +276,13 @@ export const RentProvider = ({children}) => {
         searchText,
         setSearchText,
         authModalType,
-        setAuthModalType
-      }}>
+        setAuthModalType,
+      }}
+    >
       {children}
     </RentContext.Provider>
-  )
-}
+  );
+};
 
 export const useRent = () => {
   const context = useContext(RentContext);
@@ -285,8 +307,8 @@ export const useRent = () => {
     searchText: context.searchText,
     setSearchText: context.setSearchText,
     authModalType: context.authModalType,
-    setAuthModalType: context.setAuthModalType
-  }
-}
+    setAuthModalType: context.setAuthModalType,
+  };
+};
 
-export default useRent
+export default useRent;
